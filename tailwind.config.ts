5d04319befbdd551c565011992a88889668bfe1c
Ahmad Muhammad <theahmadm3@gmail.com>
2025-02-18 06:14:58 +0100

    fonts and screen settings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,27 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				background: "var(--background)",
+				foreground: "var(--foreground)",
+			},
+			fontFamily: {
+				comfortaa: ["Comfortaa", "sans-serif"],
+			}
+		},
+		screens: {
+			mobile: { max: "767px" },
+			md: "768px",
+			lg: "976px",
+			xl: "1440px",
+		},
+	},
+	plugins: [],
 } satisfies Config;
